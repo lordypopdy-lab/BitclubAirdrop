2d805afe8256683fbce9fd1912275d9dad08b33c
lordypopdy-lab <deelordpopdy2@gmail.com>
2025-02-06 02:10:03 -0800

    safe

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 createRoot(document.getElementById('root')).render(
-  <TonConnectUIProvider manifestUrl="https://telegram-user-ashen.vercel.app/tonconnect-manifest.json">
+  <TonConnectUIProvider manifestUrl="https://bitclub-airdrop.vercel.app/tonconnect-manifest.json">
   <StrictMode>
     <App />
   </StrictMode>
